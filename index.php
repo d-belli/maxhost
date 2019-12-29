a5fafc278dddfd79f4997a88bb8b6b227415263a
dBelli <andreanimado@gmail.com>
2019-12-29 16:10:20 -0300

    mudando index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+    <meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Teste Bluma!</title>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css">
+    
+</head>    
+    
+<body>
+    
+    <div>
+        <section class = "hero is-primary is-medium is-bold">
+        <div class = "hero-head">
+            <nav class = "navbar">
+                <div class = "container">
+                    <div class = "navbar-brand">
+                        <a class = "navbar-item"> 
+                            <img src="img/logo.png">
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        </section>
+    </div>
+</body>    
+
+</html>
